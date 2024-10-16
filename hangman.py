f9cd1b069d8db35174e77c2dc1343c6ec3792020
LearnFirst <learnfirst@github.com>
2024-10-16 16:01:30 +0600

    Simple hangman

--- hangman.py
+++ hangman.py
@@ -1,21 +1,16 @@
 import random
 
-print("Welcome to the guessing Game")
-
+print("Welcome to guessing game!")
 
 word_list = ["Trojan", "Worm", "Malware", "Virus"]
 secret_word = random.choice(word_list)
-display_word = []
+
+print(secret_word)
+
+guess = str(input("Enter your guess: "))
+
 for letter in secret_word:
-    display_word += "_"
-print(display_word)
-
-
-guess = str(input("Guess a letter: ")).lower()
-
-
-for position in range(len(secret_word)):
-    letter = secret_word[position]
     if letter == guess:
-        display_word[position] = letter
-print(display_word)
+        print("Right")
+    else:
+        print("Wrong")
